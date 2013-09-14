24bdb938d28506e3220a039e3aae173540a83c17
James Flynn <jtflynnz@live.com>
2013-09-14 04:04:11 -0400

    Adds Mic Stuff

--- Immersify/MainPage.xaml.cs
+++ Immersify/MainPage.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Windows.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,14 +17,57 @@ namespace Immersify
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
+        Microphone microphone = Microphone.Default;
+        byte[] buffer;
+        MemoryStream stream = new MemoryStream();
+        SoundEffect sound;
+       
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+
+            // Timer to simulate the XNA Game Studio game loop (Microphone is from XNA Game Studio)
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(33);
+            dt.Tick += delegate { try { FrameworkDispatcher.Update(); } catch { } };
+            dt.Start();
+            microphone.BufferReady += new EventHandler<EventArgs>(microphone_BufferReady);
         }
+
+        void microphone_BufferReady(object sender, EventArgs e)
+        {
+            microphone.GetData(buffer);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        private void recordButton_Click(object sender, RoutedEventArgs e)
+        {
+            microphone.BufferDuration = TimeSpan.FromMilliseconds(1000);
+            buffer = new byte[microphone.GetSampleSizeInBytes(microphone.BufferDuration)];
+            microphone.Start();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (microphone.State == MicrophoneState.Started)
+            {
+                microphone.Stop();
+            }
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            sound = new SoundEffect(stream.ToArray(), microphone.SampleRate, AudioChannels.Mono);
+            sound.Play();
+        }
+
+
+
+     
 
         private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
         {
